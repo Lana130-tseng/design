b2b492178f4dd1ab599ef71e7db044850598fe42
Lana130-tseng <yvm0930@gmail.com>
2024-12-18 23:49:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ class Player {
         this.images = [];
         for (let i = 1; i <= this.frameCount; i++) {
             const img = new Image();
-            img.src = `https://raw.githubusercontent.com/Lana130-tseng/design/main/a${i}.png`;
+            img.src = "https://raw.githubusercontent.com/Lana130-tseng/design/main/a${i}.png"/;
             this.images.push(img);
         }
         this.currentImage = this.images[0];
