9004769332149dba7eab6c45b287a892840f92c6
Lana130-tseng <yvm0930@gmail.com>
2024-12-19 00:10:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ class Player {
         this.images = [];
         for (let i = 1; i <= this.frameCount; i++) {
             const img = new Image();
-            img.src = 'https://raw.githubusercontent.com/Lana130-tseng/design/main/a${file=i:i}.png';
+            img.src = 'https://raw.githubusercontent.com/Lana130-tseng/design/main/a${file=i}.png';
             this.images.push(img);
         }
         this.currentImage = this.images[0];
@@ -148,7 +148,7 @@ window.addEventListener('keyup', (e) => {
 });
 
 // 遊戲主迴圈
-function gameLoop() {
+function gameLoop() {a${file=i}
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // 更新玩家位置
